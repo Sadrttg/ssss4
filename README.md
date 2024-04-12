924cf0bf707fd1a903380ffa9ff253b17cf035ec
Sadrttg <166357108+Sadrttg@users.noreply.github.com>
2024-04-12 04:14:53 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,44 @@
+#### YouTube Video Download Bot
+###### This A simple YouTube Video Download Telegram Bot
+
+
+![logo](https://graph.org/file/754b7faa1308a13fc917f.jpg)
+
+
+# How To Deploy
+
+<b><details><summary>Tap To watch</summary>
+
+### Heroku Video
+<a href="https://youtu.be/ms_ApEgb0SA?feature=shared"><img alt="how to create" src="https://img.shields.io/badge/-YouTube-red?style=for-the-badge&logo=youtube&logoColor=white"/></a> 
+
+### Render Video
+<a href="https://youtu.be/A4l6LSPi-lM?feature=shared"><img alt="how to create" src="https://img.shields.io/badge/-YouTube-red?style=for-the-badge&logo=youtube&logoColor=white"/></a>
+
+</b>
+</details>
+
+## Environment Variable
+
+* `APP_ID` Get it From mytelegram.org
+
+* `API_HASH` Get it From mytelegram.org
+
+* `BOT_TOKEN` Get it from [@Botfather](https://t.me/botfather)
+
+
+### Bot Commands 
+```
+start - Check Bot Online 🔔
+help - How To Use The Bot 🆘
+
+```
+
+### My Community Details
+
+
+- YouTube Channel : [Telegram Bots 🤖](https://youtube.com/@NTBOT?feature=shared)
+- Telegram Channel : [NT Bots ❤️‍🩹](https://t.me/xvx557)
+- Telegram Group : [NT Bots Support 🎗️](https://t.me/ghyy654tbot)
+- URL Uploader Bot : [Uploader Bot 🚀](https://t.me/UploadLinkToFileBot)
+- My Tg Id : [Lisa 👑](https://t.me/xvx557)
---
+++ bot.py
@@ -0,0 +1,69 @@
+
+# ©️ LISA-KOREA | @sww80 | xvx557
+from pyrogram import Client, filters
+from pytube import YouTube
+import asyncio
+
+# Replace 'YOUR_API_ID', 'YOUR_API_HASH', and 'YOUR_BOT_TOKEN' with your actual values
+
+API_ID = '27285563'
+API_HASH = 'b236408e824fdfc2aa90812b338843cb'
+BOT_TOKEN = '7137374265:AAEQsRKIVkAHXauAjU4Cdlh5x-uCC7edi-w'
+
+# Create a Pyrogram client
+app = Client("my_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+
+# Start command handler
+@app.on_message(filters.command("start"))
+def start(client, message):
+    user = message.from_user
+    message.reply_text(f"Hello, @{user.username}!\n\nSend me the YouTube link of the video you want to upload.")
+
+# Help command handler
+@app.on_message(filters.command("help"))
+def help(client, message):
+    help_text = """
+    Welcome to the YouTube Video Uploader Bot!
+
+To upload a YouTube video, simply send me the YouTube link.
+    
+Enjoy using the bot!
+
+   ©️ Channel : @xvx557
+    """
+    message.reply_text(help_text)
+
+# Message handler for processing YouTube links
+@app.on_message(filters.regex(r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'))
+async def process_youtube_link(client, message):
+    youtube_link = message.text
+    try:
+        # Downloading text message
+        downloading_msg = await message.reply_text("Downloading video...")
+
+        # Download the YouTube video
+        yt = YouTube(youtube_link)
+        video = yt.streams.filter(progressive=True, file_extension='mp4').first()
+        video.download(filename='downloaded_video.mp4')
+
+        # Uploading text message
+        uploading_msg = await message.reply_text("Uploading video...")
+
+        # Send the video file to the user
+        sent_message = await app.send_video(message.chat.id, video=open('downloaded_video.mp4', 'rb'), caption=yt.title)
+
+        # Delay for a few seconds and delete downloading and uploading
+        await asyncio.sleep(2)
+        await downloading_msg.delete()
+        await uploading_msg.delete()
+
+        # Send successful upload message
+        await message.reply_text("\n\nمطور البوت : @sww80\n\nتم التحميل بنجاح !")
+
+    except Exception as e:
+        error_text = 'Error: Failed to process the YouTube link. Please make sure the link is valid and try again.'
+        await message.reply_text(error_text)
+
+# Start the bot
+print("🎊 sajjad 🎊")
+app.run()
